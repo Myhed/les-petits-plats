8ffc797fa1e88f62060c2aa923e396103cace907
Naytchead <benrhouma.myhed@gmail.com>
2023-10-25 19:55:52 +0200

    [Search entities] finished to implement search entities with small usecase bar DOM

--- src/entities/search.js
+++ src/entities/search.js
@@ -6,51 +6,85 @@ export class Search {
         this.results = [];
     }
 
-    recipe(index){
+    _recipe(index){
         return {
             appliances: this.recipes.getAppliances[index],
             ingredients: this.recipes.getIngredients[index],
             ustensils: this.recipes.getUstensils[index],
-            ...this.recipes.getPresentations[index],
+            ...this.recipes.getPresentations[index]
         }
     }
+    onWord(word, fields = {titles: "ig", descriptions:"mig", ingredients:null}){
+        const fieldsKey = Object.keys(fields)
+        const fieldsValue = Object.values(fields);
+        word = word.trim();
 
-    onTitles(searched){
+        fieldsKey.forEach((key, index) => {
+            const nameFunc = key.substr(0,1).toUpperCase() + key.substr(1, key.length - 1);
+            if(nameFunc === 'Ingredients' || nameFunc === 'Ustensils') {
+                this[`_on${nameFunc}`](word)
+            }else {
+                const regexSearched = new RegExp(word, `${fieldsValue[index]}`);
+                console.log(`_on${key}`)
+                this[`_on${nameFunc}`](regexSearched)
+            }
+            
+        });
+        console.log('results on:', this.results)
+        return this;
+    }
+
+    _onTitles(regexSearch){
         // console.log('search on title: ',this.recipes.getPresentations)
-        const regexSearched = new RegExp(searched, "i");
         const titlesMatched = this.recipes.getPresentations
-            .filter(({name}, index) => name.match(regexSearched) ? this.recipe(index): null)
+            .map(({name}, index) => name.match(regexSearch) ? this._recipe(index): null)
         console.log('titleMatched: ', titlesMatched);
         this.results = this.results.concat(titlesMatched);
         return this;
     }
 
-    onDescriptions(searched){
-        const regexSearched = new RegExp(searched, "m");
+    _onDescriptions(regexSearch){
         const descriptionMatched = this.recipes.getPresentations
-            .filter(({description},index) => description.match(regexSearched) ? this.recipe(index): null)
-        // console.log('descriptionMatched :' , descriptionMatched)
+            .map(({description, id}) => description.match(regexSearch) ? this._recipe(id): null)
+        console.log('descriptionMatched :' , descriptionMatched)
         this.results = this.results.concat(descriptionMatched);
         return this;
     }
 
-    onIngredients(searched){
-        searched = searched.substr(0,1).toUpperCase() + searched.substr(1, searched.length-1);
+    _onIngredients(word){
+        word = word.substr(0,1).toUpperCase() + word.substr(1, word.length-1);
         // console.log('searched: ', searched);
         // console.log('getIngredients: ', this.recipes.getIngredients);
         const ingredientsMatched = this.recipes.getIngredients
         .map((ingredients) => ingredients
             .map(({ingredient}) => ingredient))
-        .map((ingredient,index) => ingredient.includes(searched) ? 
-                this.recipe(index): null)
+        .map((ingredient,index) => ingredient.includes(word) ? 
+                this._recipe(index): null)
             .filter(presentation => presentation)
-        // console.log('ingredientsMatched :' , ingredientsMatched)
+        console.log('ingredientsMatched :' , ingredientsMatched)
         this.results = this.results.concat(ingredientsMatched);
         return this;
     }
 
+    onUstensils(ustensil){
+        const ustensilsMatched = this.recipes.getUstensils
+            .map((ustensils, index) => ustensils.includes(ustensil) ? this._recipe(index): null)
+        console.log('ustensilsMatched:', ustensilsMatched)
+        return this;        
+    }
+
+    onAppliances(regexSearch){
+        const appliancesMatched = this.recipes.getAppliances
+            .map((appliance, index) => appliance.match(regexSearch) ? this._recipe(index): null)
+        console.log('appliancesMatched', appliancesMatched);
+        return this;
+    }
+
     get getResults(){
-        return this.results
+        const results = this.results
+        .filter(result => result != null)
         .reduce((acc, result) => (acc[result.id] = result, acc), {})
+        this.results = [];
+        return results;
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@
 import { getRecipes } from './helpers/datas.js';
 import {AdvancedSelect, Form, Recipies, Search} from './entities/index.js';
 
-getRecipes().then(({recipes: recipesList}) => {
+const recipes = getRecipes().then(({recipes: recipesList}) => {
     const recipes = new Recipies(recipesList);
     const advancedSelect = new AdvancedSelect(recipes);
     const form = new Form(advancedSelect);
     const search = new Search(form, recipes);
-    console.log('results: ',search
-    .onTitles('limonade')
-    .onDescriptions('limonade')
-    .onIngredients('Lait de coco')
-    .getResults);
-    console.log('form: ', form);
-    console.log('search: ', search);
+    console.log('results: ',search.onUstensils('cuillère à Soupe').onAppliances('Blender'))
+    // .onTitles('limonade')
+    // .onDescriptions('limonade')
+    // .onIngredients('Lait de coco')
+    // .onWord('')
+    // .getResults);
+    
+    return search;
+})
+
+const searchDom = document.querySelector('#search')
+
+searchDom.addEventListener('keyup', async (e) => {
+    const search = await recipes;
+    const searchedDom = e.target.value;
+    if(searchedDom.length >= 3){
+        console.log('searched: ', searchedDom)
+        const results = search
+        .onWord(searchedDom)
+        .getResults
+
+        console.log('results searched: ', results);
+    }
+    console.log('e:', e.target.value)
 })
