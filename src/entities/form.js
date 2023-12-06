b3099893f286abd378d046038f2aafa21ff0159f
Myhed <benrhouma.myhed@gmail.com>
2023-12-06 17:53:49 +0100

    [Form] fix search form entities store previous search at each time event capture search

--- src/entities/form.js
+++ src/entities/form.js
@@ -1,6 +1,7 @@
-import { renderCardsRecipes, reRenderCardsRecipes } from '../utils/render.js';
+import { renderCardsRecipes } from '../utils/render.js';
 import { AdvancedSelect } from './advancedSelect.js';
 import { Recipies } from './recipies.js';
+import { Search } from './search.js';
 
 
 export class Form {
@@ -9,8 +10,12 @@ export class Form {
         this.ustensils = Object.values(advancedSelect.getUstensils);
         this.appliances = Object.values(advancedSelect.getAppliances);
         this.ingredients = Object.values(advancedSelect.getIngredients);
+        this.allRecipes = search.getRecipes;
         this.labels = [];
+        this.labelsDom = [];
+        this.searchs = [];
         this.search = search;
+        this.nextSearch;
     }
 
     ingredientsSelect(ingredients){
@@ -18,9 +23,18 @@ export class Form {
         const ingredientSelect = document.querySelector('.ingredient');
         ingredientSelect.innerHTML = `
         <ul class="p-3">
-            <li data-span-name="ingredient" class="list-group-item"><input type="text" class="form-control"></li>
+            <li data-input-id="ingredient" class="list-group-item"><input type="text" class="form-control"></li>
         </ul>
-        ${ingredients.map(ingredient => `<li data-span-name="ingredient"><a class='dropdown-item'>${ingredient}</a></li>`).join('')}
+        ${
+            // console.log(this.labels),
+            this.labels.length > 0 ? ingredients
+            .map(ingredient =>  this.labels.reduce((acc, label) => (!Object.values(label).includes(ingredient) ? acc = ingredient: null),''))
+            .filter(HTMLElement => HTMLElement !== null)
+            .map(ingredient => `<li data-span-name="ingredient"><a class='dropdown-item'>${ingredient}</a></li>`)
+            .join('')
+            :ingredients
+            .map(ingredient => `<li data-span-name="ingredient"><a class='dropdown-item'>${ingredient}</a></li>`)
+            .join('')}
         `
         return this;
     }
@@ -30,9 +44,17 @@ export class Form {
         const applianceSelect = document.querySelector('.appliances');
         applianceSelect.innerHTML = `
         <ul class="p-3">
-            <li data-span-name-="appliance" class="list-group-item"><input type="text" class="form-control"></li>
+            <li data-id-input="appliance" class="list-group-item"><input type="text" class="form-control"></li>
         </ul>
-        ${appliances.map(appliance => `<li data-span-name="appliance"><a class='dropdown-item'>${appliance}</a></li>`).join('')}
+        ${this.labels.length > 0 ? appliances
+        .map(appliance =>  this.labels.reduce((acc, label) => (label.name !== appliance ? acc = appliance: null),''))
+        .filter(HTMLElement => HTMLElement !== null)
+        .map(appliance => `<li data-span-name="appliance"><a class='dropdown-item'>${appliance}</a></li>`)
+        .join('')
+        :appliances
+        .map(appliance => `<li data-span-name="appliance"><a class='dropdown-item'>${appliance}</a></li>`)
+        .join('')
+        }
         `
         return this;
     }
@@ -42,81 +64,151 @@ export class Form {
         const ustensilSelect = document.querySelector('.ustensils');
         ustensilSelect.innerHTML = `
         <ul class="p-3">
-            <li data-span-name="ustensil" class="list-group-item"><input type="text" class="form-control"></li>
+            <li data-id-input="ustensil" class="list-group-item"><input type="text" class="form-control"></li>
         </ul>
-        ${ustensils.map(ustensil => `<li data-span-name="ustensil"><a class='dropdown-item'>${ustensil}</a></li>`).join('')}
+        ${
+        this.labels.length > 0 ? ustensils
+        .map(ustensil =>  this.labels.reduce((acc, label) => (label.name !== ustensil ? acc = ustensil: null),''))
+        .filter(HTMLElement => HTMLElement !== null)
+        .map(ustensil => `<li data-span-name="ustensil"><a class='dropdown-item'>${ustensil}</a></li>`)
+        .join('')
+        :ustensils
+        .map(ustensil => `<li data-span-name="ustensil"><a class='dropdown-item'>${ustensil}</a></li>`)
+        .join('')
+        }
         `
         return this;
     }
 
     searchBar(parent){
-        const searchDom = document.querySelector('#search')
+        this.searchDom = document.querySelector('#search')
 
         this.
             ingredientsSelect()
             .appliancesSelect()
             .ustensilsSelect()
         
-        searchDom.addEventListener('keyup', (e) => {
+        this.searchDom.addEventListener('keyup', (e) => {
             const searchedDom = e.target.value.trim().toLowerCase();
             const numLetters = searchedDom.length;
-            let recipes;
-            let advancedSelect;
             switch(numLetters){
+                // case 1:
+                //     this.clearLabels(parent);
+                // break;
                 case 3:
-                    const { results } = this.search
+                    const search = 
+                    this.nextSearch||this.search;
+                    const results = search
                     .onWord(searchedDom)
-                    .getResults;
+                    .getResults.results;
+
+                    console.log('search is: ', search);
                     const entriesFound = Object.values(results);
-                    recipes = new Recipies(entriesFound);
-                    advancedSelect = new AdvancedSelect(recipes);
-                    this
-                     .ingredientsSelect(Object.values(advancedSelect.getIngredients))
-                     .appliancesSelect(Object.values(advancedSelect.getAppliances))
-                     .ustensilsSelect(Object.values(advancedSelect.getUstensils))
-                    renderCardsRecipes(entriesFound, parent);
+                    this.rebuild(parent, entriesFound);
                  break;
-                case 0:
-                    const recipesList = this.search.getRecipes;
-                    recipes = new Recipies(recipesList);
-                    advancedSelect = new AdvancedSelect(recipes);
-                    this
-                    .ingredientsSelect(Object.values(advancedSelect.getIngredients))
-                    .appliancesSelect(Object.values(advancedSelect.getAppliances))
-                    .ustensilsSelect(Object.values(advancedSelect.getUstensils))
-                    console.log('recipes: ',recipes)
-                    renderCardsRecipes(recipesList, parent);
+                 case 0:
+                    let nextSearch;
+                    if(this.searchs.length > 2){
+                        nextSearch = this.searchs[this.searchs.length - 2];
+                    }else{
+                       nextSearch = this.nextSearch;
+                    }
+                     const entriesSearchBar = Object.values(nextSearch.getResults.results);
+                     const recipes = entriesSearchBar.length > 0 ? entriesSearchBar : this.search; 
+                     this.labelsIsEmpty(recipes, parent);
                 break;
             }
         })
     }
 
-    createLabelSelect(nameItemSelected){
-     this.labels = [...this.labels, `
-     <button type="button" class="btn btn-warning w-15">
-         <span class="text-white">${nameItemSelected}</span> <i class="fa-solid fa-xmark text-light"></i> 
-      </button>
-     `];
-     
-     return this.labels;
+    rebuild(parent,recipiesList){
+        console.log('recipiesList:', recipiesList);
+        const recipes = new Recipies(recipiesList);
+        const advancedSelect = new AdvancedSelect(recipes);
+        const search = new Search(recipes);
+        if(recipiesList.length > 1){
+            this
+                .ingredientsSelect(Object.values(advancedSelect.getIngredients))
+                .appliancesSelect(Object.values(advancedSelect.getAppliances))
+                .ustensilsSelect(Object.values(advancedSelect.getUstensils))
+         this.searchs = [...this.searchs, search];
+        }else(
+            this
+                .ingredientsSelect([])
+                .appliancesSelect([])
+                .ustensilsSelect([])
+        )
+        renderCardsRecipes(recipiesList, parent);
+        this.nextSearch = recipiesList.length > 0 ? search:this.nextSearch;
+        this.searchSelect(parent, this.nextSearch);
     }
-    searchSelect(){
+
+    createLabelSelect(nameItemSelected, funcName){
+        this.labels = [...this.labels, {name: nameItemSelected, funcName}];
+        this.labelsDom = [...this.labelsDom, `
+        <button type="button" class="btn btn-warning w-15">
+            <span class="text-white">${nameItemSelected}</span> <i class="fa-solid fa-xmark text-light"></i> 
+         </button>
+        `];
+     return this.labelsDom;
+    }
+
+    clearLabels(parent){
+        const labelSelect = document.querySelector('#label-select');
+        this.labels = [];
+        this.labelsDom = [];
+        labelSelect.innerHTML = '';
+        this.rebuild(parent, this.allRecipes);
+    }
+
+    labelsIsEmpty(searched, parent){
+        if(this.labels.length > 0){
+            const recipeListsWithLabels = Array(this.labels.length).fill(null)
+            .map((_, index) => searched[this.labels[index].funcName](this.labels[index].name).getResults.results)
+            .reduce((acc, recipes) => (acc = acc.concat(Object.values(recipes)), acc), []);
+            const searchWithLabels = Object.values(searched.distinct(recipeListsWithLabels));
+            // console.log('labels: ',this.labels);
+            this.rebuild(parent, searchWithLabels);
+        }else{
+            // console.log('no labls')
+            // console.log('allrecipes: ',this.allRecipes);
+            const valueSearchBar = this.searchDom.value
+            const recipes = valueSearchBar.length > 0 ? Object.values(this.search.onWord(valueSearchBar).getResults.results) : this.allRecipes;
+            this.rebuild(parent, recipes);
+        }
+    }
+
+    deleteLabel(index, faxmark ,{ul, li, labelSelect, searched, parent}){
+            this.labels = this.labels.filter(label => this.labels[index].name !== label.name)
+            console.log('labels: ',this.labels);
+            this.labelsDom.splice(index, 1);
+            ul.appendChild(li);
+            labelSelect.removeChild(faxmark.parentNode);
+            this.labelsIsEmpty(searched, parent);
+    }
+
+    onClickCrossIcon(HtmlElementList){
+        const faxmarks = document.querySelectorAll('.fa-xmark');
+        faxmarks.forEach((faxmark, index) => faxmark
+            .addEventListener('click', () => this.deleteLabel(index, faxmark, HtmlElementList)));
+    }
+
+    searchSelect(parent,searched){
+        searched = searched || this.search;
+        console.log('searched:', searched);
         const dropdowns = document.querySelectorAll('.dropdown-menu li a');
         const labelSelect = document.querySelector('#label-select');
         dropdowns.forEach(a => {
             a.addEventListener('click', (e) => {
                 const ul = a.parentNode.parentNode;
                 const li = a.parentNode;
-                labelSelect.innerHTML = `${this.createLabelSelect(a.textContent).map(label => label).join(' ')}`
-                const faxmarks = document.querySelectorAll('.fa-xmark');
-                faxmarks.forEach((faxmark, index) => {
-                    faxmark.addEventListener('click', (e)=>{
-                        this.labels.splice(index, 1);
-                        ul.appendChild(li);
-                        labelSelect.removeChild(faxmark.parentNode);
-                    })
-                });
-                ul.removeChild(li);
+                const spanName =  li.dataset.spanName;
+                const chunkNameFunc = spanName.substr(0,1).toUpperCase() +  spanName.substr(1,spanName.length - 1);
+                labelSelect.innerHTML = `${this.createLabelSelect(a.textContent, `entriesOn${chunkNameFunc}s`).map(label => label).join(' ')}`
+                const recipesList = Object.values(searched[`entriesOn${chunkNameFunc}s`](a.textContent).distinct());
+                this.rebuild(parent,recipesList);
+                console.log(document.querySelectorAll(`input[data-id-input=${spanName}]`));
+                this.onClickCrossIcon({ul, li, labelSelect, searched, parent});
             }, false);
         });
     }
--- src/utils/render.js
+++ src/utils/render.js
@@ -1,31 +1,36 @@
 import { cardBody, cardDom, layoutElementIngredient, layoutIngredient } from "../components/index.js";
 
-export const reRenderCardsRecipes  = (entriesFound, parent) => (entriesFound
-.map((recipe, i) => (
-    console.log('name ',recipe),
-    recipe.ingredients = recipe.ingredients
-    .reduce((acc, _, j) => {
-        if(j%2){
-            if(entriesFound[i].ingredients[j+1]){
-                acc = [...acc, [entriesFound[i].ingredients[j], 
-                entriesFound[i].ingredients[j+1]]] 
-            }else{
-                acc = [...acc, [entriesFound[i].ingredients[j]]] 
-            }
-        }
-    return acc;
-    },[]), 
-recipe))
-.map((recipe, i) => (
-    recipe.ingredients = layoutElementIngredient(recipe.ingredients),
-       recipe.ingredients = layoutIngredient(recipe.ingredients),
-       recipe.ingredients = cardBody('ingredients',recipe.ingredients),
-    cardDom({name: recipe.name, description: recipe.description, image: recipe.image}, recipe.ingredients))),
-parent.innerHTML = cards.join(''));
+// export const renderCardsRecipes  = (entriesFound, parent) => {
+    
+    
+// const t = entriesFound
+// .map((recipe, i) => (
+//     console.log('name ',recipe),
+//     recipe.ingredients = recipe.ingredients
+//     .reduce((acc, _, j) => {
+//         if(j%2){
+//             if(entriesFound[i].ingredients[j+1]){
+//                 acc = [...acc, [entriesFound[i].ingredients[j], 
+//                 entriesFound[i].ingredients[j+1]]] 
+//             }else{
+//                 acc = [...acc, [entriesFound[i].ingredients[j]]] 
+//             }
+//         }
+//     return acc;
+//     },[]), 
+// recipe))
+// .map((recipe, i) => (
+//     recipe.ingredients = layoutElementIngredient(recipe.ingredients),
+//        recipe.ingredients = layoutIngredient(recipe.ingredients),
+//        recipe.ingredients = cardBody('ingredients',recipe.ingredients),
+//     cardDom({name: recipe.name, description: recipe.description, image: recipe.image}, recipe.ingredients)))
+// parent.innerHTML = t.join('')
+
+// };
 
 
 export const renderCardsRecipes = (recipes, parent) => {
-    const t = recipes.map((recipe, i) => (
+    const t = structuredClone(recipes).map((recipe, i) => (
         recipe.ingredients = recipe.ingredients
         .reduce((acc, _, j) => {
             if(j%2){
