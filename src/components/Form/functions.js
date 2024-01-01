7c543af3dbabe1c36c1a2ecafbcd6ed05c911435
Myhed <benrhouma.myhed@gmail.com>
2024-01-01 08:06:31 +0100

    [searchSelect] add event listener on all options selects

--- src/components/Form/functions.js
+++ src/components/Form/functions.js
@@ -30,30 +30,37 @@ const whichSelect= (name) => {
     return document.querySelector(`.dropdown-menu${name}`);
 }
 
-const selects = (advancedSelect) => {
-    const nameSelects = Object.keys(advancedSelect);
+const selects = (advancedSelect, cb) => {
+    const nameSelects = Object.keys(advancedSelect)
     for(let s = 0; s < nameSelects.length; s++){
-        const select = whichSelect(`.${nameSelects[s]}`);
+        const select = whichSelect(`.${nameSelects[s]}`)
         select.innerHTML = `
         <ul class="p-3">
             <li data-id-input="${nameSelects[s]}" class="list-group-item"><input type="text" class="form-control"></li>
         </ul>
-        ${(options(nameSelects[s], advancedSelect).join(''))}
         `
+        options(nameSelects[s], advancedSelect, select, cb)
     }
 
 }
 
-const options = (name, advancedSelect) => {
+const options = (name, advancedSelect, select, cb) => {
     const nameMethodeGet = name.substr(0,1).toUpperCase() + name.substr(1,name.length - 1);
-    const selectOptions = advancedSelect[`get${nameMethodeGet}`]();
-    let selectOptionsDom = [];
-    for(let o = 0; o < selectOptions.length; o++){
-        selectOptionsDom = [...selectOptionsDom, `<li data-span-name="${name}"><a class='dropdown-item'>${selectOptions[o]}</a></li>`];
+    const selectOptions = advancedSelect[`get${nameMethodeGet}`]()
+    for(let o = 0; o <= selectOptions.length; o++){
+        const li = `<li data-span-name="${name}"><a class='dropdown-item'>${selectOptions[o]}</a></li>`
+        const parser = new DOMParser().parseFromString(li, "text/xml")
+        parser.children[0].addEventListener('click', (e) => cb(e), false)
+        select.appendChild(parser.children[0])
     }
-    return selectOptionsDom
 }
 
-export const searchSelect = function(){
-    selects(this.advancedSelect);
+export const searchSelect = function(advancedSelect, cb){
+    advancedSelect = advancedSelect || this.advancedSelect;
+    selects(this.advancedSelect, (e => {
+        const li = e.target.parentNode;
+        console.log(li);
+    }));
+
+
 }
